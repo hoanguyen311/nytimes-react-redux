99315570faa72ab08b91caecf4f42f7fbd75d22d
Harry Nguyen <hoa.nguyen-thai@lazada.com>
2018-10-17 23:00:09 +0700

    basename={process.env.PUBLIC_URL}

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -25,7 +25,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Layout>
           <Header className="app-header">
             <h1 className="app-headline">
